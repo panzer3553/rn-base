cb927f94cf89e0918fcba71d4b78157c3764a8d1
Hoang Minh Ho <hoang.ho@enouvo.com>
2016-04-27 14:40:12 +0700

    Clean code

--- App/Containers/SwiperScreen.js
+++ App/Containers/SwiperScreen.js
@@ -4,193 +4,12 @@ import { Colors, Images, Metrics } from '../Themes'
 import Icon from 'react-native-vector-icons/Ionicons'
 import ModalPicker from 'react-native-modal-picker'
 import { connect } from 'react-redux'
-
-var styles = StyleSheet.create({
-    containerModal: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: Colors.snow
-  },
-  innerContainer: {
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-    container:{
-    height:Metrics.screenHeight,
-    width:Metrics.screenWidth
-  },
-  backgroundFixed: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    backgroundColor: '#F67C01'
-  },
-  sliders: {
-    position: 'absolute',
-    width: Metrics.screenWidth,
-    bottom: 70,
-    left:0
-  },
-  slide: {
-    flex: 1,
-    height: Metrics.screenHeight -200,
-    alignItems: 'center',
-    paddingBottom:50
-  },
-  slideText:{
-    color: "#fff",
-    textAlign:"center"
-  },
-  slideTextTitle:{
-    color: "#fff",
-    textAlign:"center",
-    fontWeight:"700"
-  },
-  logo:{
-    alignItems:"center",
-    position:"absolute",
-    width: Metrics.screenWidth,
-    top: 50,
-    left: 0,
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"center"
-  },
-  logoText:{
-    color:"#fff",
-    textAlign:"left",
-    fontSize: 35,
-    fontWeight:"700",
-    backgroundColor:"transparent",
-  },
-  logoIconContainer:{
-    backgroundColor:"transparent",
-    paddingRight:5,
-    marginTop:5,
-    marginBottom: 40
-  },
-  logoTextContainer:{
-    backgroundColor:"transparent"
-  },
-  btnContainer:{
-    position:"absolute",
-    width: Metrics.screenWidth,
-    bottom: 0,
-    left: 0,
-    height: 40,
-    flexDirection: "row"
-  },
-  btn:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center"
-  },
-  btnText:{
-    color:"#fff",
-    fontWeight:"500",
-    fontSize:14
-  },
-  pickerContainer: {
-    marginTop: 16,
-    width: Metrics.screenWidth * 2 / 3,
-    backgroundColor: Colors.snow,
-    padding: 4,
-    paddingLeft: 8,
-    flexDirection: "row"
-  },
-  dropDownIcon: {
-    position: 'absolute',
-    right: 8  
-  },
-    modalButton: {
-    marginTop: 10,
-  },
-    separator: {
-    height: 1,
-    backgroundColor: '#CCCCCC',
-  },
-    row: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    padding: 10,
-    backgroundColor: '#F6F6F6',
-  },
-    text: {
-      flex: 1,
-    }
-})
-
-const cities = [{ "name" : "An Giang" , "cityCode" : 8476 },
-{ "name" : "Bà Rịa - Vũng Tàu" , "cityCode" : 8464 },
-{ "name" : "Bắc Giang" , "cityCode" : 84240 },
-{ "name" : "Bắc Kạn" , "cityCode" : 84281 },
-{ "name" : "Bạc Liêu" , "cityCode" : 84781 },
-{ "name" : "Bắc Ninh" , "cityCode" : 84241 },
-{ "name" : "Bến Tre" , "cityCode" : 8475 },
-{ "name" : "Bình Định" , "cityCode" : 8456 },
-{ "name" : "Bình Dương" , "cityCode" : 84650 },
-{ "name" : "Bình Phước" , "cityCode" : 84651 },
-{ "name" : "Bình Thuận" , "cityCode" : 8462 },
-{ "name" : "Cà Mau" , "cityCode" : 84780 },
-{ "name" : "Cần Thơ" , "cityCode" : 84710 },
-{ "name" : "Cao Bằng" , "cityCode" : 8426 },
-{ "name" : "Đà Nẵng" , "cityCode" : 84511 },
-{ "name" : "Đắk Lắk" , "cityCode" : 84500 },
-{ "name" : "Đắk Nông" , "cityCode" : 84501 },
-{ "name" : "Điện Biên" , "cityCode" : 84230 },
-{ "name" : "Đồng Nai" , "cityCode" : 8461 },
-{ "name" : "Đồng Tháp" , "cityCode" : 8467 },
-{ "name" : "Gia Lai" , "cityCode" : 8459 },
-{ "name" : "Hà Giang" , "cityCode" : 84219 },
-{ "name" : "Hà Nam" , "cityCode" : 84351 },
-{ "name" : "Hà Nội" , "cityCode" : 844   },
-{ "name" : "Hà Tĩnh" , "cityCode" : 8439 },
-{ "name" : "Hải Dương" , "cityCode" : 84320 },
-{ "name" : "Hải Phỏng" , "cityCode" : 8431 },
-{ "name" : "Hậu Giang" , "cityCode" : 84711 },
-{ "name" : "Hòa Bình" , "cityCode" : 84218 },
-{ "name" : "Hồ Chí Minh" , "cityCode" : 848  },
-{ "name" : "Hưng Yên" , "cityCode" : 84321 },
-{ "name" : "Khánh Hoà" , "cityCode" : 8458 },
-{ "name" : "Kiên Giang" , "cityCode" : 8477 },
-{ "name" : "Kon Tum" , "cityCode" : 8460 },
-{ "name" : "Lai Châu" , "cityCode" : 84231 },
-{ "name" : "Lâm Đồng" , "cityCode" : 8463 },
-{ "name" : "Lạng Sơn" , "cityCode" : 8425 },
-{ "name" : "Lào Cai" , "cityCode" : 8420 },
-{ "name" : "Long An" , "cityCode" : 8472 },
-{ "name" : "Nam Định" , "cityCode" : 84350 },
-{ "name" : "Nghệ An" , "cityCode" : 8438 },
-{ "name" : "Ninh Bình" , "cityCode" : 8430 },
-{ "name" : "Ninh Thuận" , "cityCode" : 8468 },
-{ "name" : "Phú Thọ" , "cityCode" : 84210 },
-{ "name" : "Phú Yên" , "cityCode" : 8457 },
-{ "name" : "Quảng Bình" , "cityCode" : 8452 },
-{ "name" : "Quảng Nam" , "cityCode" : 84510 },
-{ "name" : "Quảng Ngãi" , "cityCode" : 8455 },
-{ "name" : "Quảng Ninh" , "cityCode" : 8433 },
-{ "name" : "Quảng Trị" , "cityCode" : 8453 },
-{ "name" : "Sóc Trăng" , "cityCode" : 8479 },
-{ "name" : "Sơn La" , "cityCode" : 8422 },
-{ "name" : "Tây Ninh" , "cityCode" : 8466 },
-{ "name" : "Thái Bình" , "cityCode" : 8436 },
-{ "name" : "Thái Nguyên" , "cityCode" : 84280 },
-{ "name" : "Thanh Hoá" , "cityCode" : 8437 },
-{ "name" : "Thừa Thiên - Huế" , "cityCode" : 8454 },
-{ "name" : "Tiền Giang" , "cityCode" : 8473 },
-{ "name" : "Trà Vinh" , "cityCode" : 8474 },
-{ "name" : "Tuyên Quang" , "cityCode" : 8427 },
-{ "name" : "Vĩnh Long" , "cityCode" : 8470 },
-{ "name" : "Vĩnh Phúc" , "cityCode" : 84211 },
-{ "name" : "Yên Bái" , "cityCode" : 8429 }]
+import cities from '../Config/CitiesData'
+import styles from './Styles/SwiperStyles.js'
 
 class Intro extends Component{
   _onMomentumScrollEnd(e, state, context) {
     // you can get `state` and `this`(ref to swiper's context) from params
-    console.log(state, context.state)
   }
 
   constructor(props) {
@@ -207,7 +26,7 @@ class Intro extends Component{
 
   renderRow(rowData: string, sectionID: number, rowID: number) {
     return (
-      <TouchableHighlight onPress={() => {this.pressRow(rowData)}} key={rowID}>
+      <TouchableHighlight onPress={() => {this.pressRow(rowData)}}>
         <View>
           <View style={styles.row}>
             <Text style={styles.text}>
@@ -307,9 +126,6 @@ class Intro extends Component{
 }
 
 export default class SwiperScreen extends Component{
-  componentDidMount(){
-    console.log(this.props)
-  }  
 
   render() {
     return(
